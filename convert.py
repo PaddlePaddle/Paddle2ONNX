4a1ad2a41a0fa044d6879a739c0b127344e9d192
Yibing Liu <liuyibing01@baidu.com>
2018-04-08 03:46:59 -0700

    Support onnx model saving

--- convert.py
+++ convert.py
@@ -18,7 +18,7 @@ import argparse
 from onnx import helper, checker
 import paddle.fluid as fluid
 
-import ops
+import fluid_onnx.ops as ops
 from fluid_onnx.variables import paddle_variable_to_onnx_tensor
 from fluid_onnx.variables import PADDLE_TO_ONNX_DTYPE
 
@@ -26,9 +26,9 @@ def parse_args():
     # Read arguments: path to model.
     parser = argparse.ArgumentParser()
     parser.add_argument("--fluid_model", required=True,
-        help="Input PaddlePaddle Fluid model")
-    parser.add_argument("--onnx_model", required=False,
-        help="Output ONNX model")
+        help="Input PaddlePaddle Fluid model.")
+    parser.add_argument("--onnx_model_dir", required=False,
+        help="The output directory for ONNX model.")
     args = parser.parse_args()
     return args
 
@@ -89,16 +89,26 @@ def convert(args):
 
         # make graph
         model_name = os.path.basename(args.fluid_model.strip('/')).split('.')[0]
-        graph = helper.make_graph(onnx_nodes, model_name, inputs, outputs)
+        onnx_graph = helper.make_graph(onnx_nodes, model_name, inputs, outputs)
 
         # make model
-        model = helper.make_model(graph, producer_name='PaddlePaddle')
+        onnx_model = helper.make_model(onnx_graph, producer_name='PaddlePaddle')
 
         # model check
-        checker.check_model(model)
+        checker.check_model(onnx_model)
 
         # output readable model
-        print("The converted model is:\n{}".format(model))
+        print("The converted model is:\n{}".format(onnx_model))
+        
+        if args.onnx_model_dir is not None:
+            if os.path.isdir(args.onnx_model_dir):
+                model_path = os.path.join(args.onnx_model_dir, 
+                    model_name+".onnx")
+                with open(model_path, 'wb') as f:
+                        f.write(onnx_model.SerializeToString())
+                print("Saved model to path: %s" % model_path)
+            else:
+                raise IOError("Invalid directory: %s" % args.onnx_model_dir)
 
 if __name__ == "__main__":
     args = parse_args()
