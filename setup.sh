3e333dae3ca0c553de53e94f6b96a0080a826ad8
yangyaming <mxscmxsc@gmail.com>
2018-04-08 23:01:48 +0800

    Add setup.sh and remove clang-format.

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# install python dependencies
+if [ -f "requirements.txt" ]; then
+    pip install -r requirements.txt
+fi
+if [ $? != 0 ]; then
+    echo "Install python dependencies failed !!!"
+    exit 1
+fi
