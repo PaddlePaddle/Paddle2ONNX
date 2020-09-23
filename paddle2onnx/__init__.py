ff178882d8d4d28bc21e89d6730d060178e89ff2
Channingss <chen_lingchi@163.com>
2020-09-23 02:33:17 +0000

    add interface for x2paddle

--- paddle2onnx/__init__.py
+++ paddle2onnx/__init__.py
@@ -14,14 +14,5 @@
 
 __version__ = "0.2.1"
 
-name_counter = dict()
-
-
-def gen_name(op_name, var_name):
-    name = "{}.{}".format(op_name, var_name)
-    if name not in name_counter:
-        name_counter[name] = 0
-    else:
-        name_counter[name] += 1
-    name = name + "." + str(name_counter[name])
-    return name
+from . import convert
+convert = convert.convert
