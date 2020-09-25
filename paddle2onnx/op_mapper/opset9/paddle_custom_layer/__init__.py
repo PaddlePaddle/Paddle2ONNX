a09adddacb875ec32c06850d3d2903d291a7cb94
Channingss <chen_lingchi@163.com>
2020-09-25 06:18:46 +0000

    fix bug for paddlex multiclass_nms

--- paddle2onnx/op_mapper/opset9/paddle_custom_layer/__init__.py
+++ paddle2onnx/op_mapper/opset9/paddle_custom_layer/__init__.py
@@ -11,3 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from . import box_coder
+from . import im2sequence
+from . import multiclass_nms
+from . import prior_box
+from . import yolo_box
