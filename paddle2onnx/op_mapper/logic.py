7e569db452aa64f79bc507b4602f6ce3698a283c
AndPuqing <1569540783@qq.com>
2021-10-03 17:26:05 +0800

    add eleven OP support and unit test

--- paddle2onnx/op_mapper/logic.py
+++ paddle2onnx/op_mapper/logic.py
@@ -87,6 +87,18 @@ class LogicalAnd():
             outputs=node.output('Out'))
 
 
+@op_mapper('logical_xor')
+class LogicalXor():
+    support_opset_verision_range = (7, 12)
+
+    @classmethod
+    def opset_6(cls, graph, node, **kw):
+        graph.make_node(
+            'Xor',
+            inputs=[node.input('X', 0), node.input('Y', 0)],
+            outputs=node.output('Out'))
+
+
 @op_mapper('less_equal')
 class LessOrEqual():
     support_opset_verison_range = (12, )
