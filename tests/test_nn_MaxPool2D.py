ba5216b684ad67a53b838a2399611e57152a2e22
huangshenghui <huangshenghui@baidu.com>
2021-12-13 10:28:34 +0800

    revise test case

--- tests/test_nn_MaxPool2D.py
+++ tests/test_nn_MaxPool2D.py
@@ -22,23 +22,16 @@ class Net(paddle.nn.Layer):
     simple Net
     """
 
-    def __init__(self,
-                 kernel_size=2,
-                 stride=None,
-                 padding=0,
-                 return_mask=False,
-                 ceil_mode=False,
-                 data_format="NCHW",
-                 name=None):
+    def __init__(self):
         super(Net, self).__init__()
         self._max_pool = paddle.nn.MaxPool2D(
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            return_mask=return_mask,
-            ceil_mode=ceil_mode,
-            data_format=data_format,
-            name=name)
+            kernel_size=2,
+            stride=None,
+            padding=0,
+            ceil_mode=False,
+            return_mask=False,
+            data_format='NCHW',
+            name=None)
 
     def forward(self, inputs):
         """
@@ -62,126 +55,3 @@ def test_MaxPool2D_base():
         paddle.to_tensor(
             randtool("float", -1, 1, [3, 1, 10, 10]).astype('float32')))
     obj.run()
-
-
-def test_MaxPool2D_base_VALID():
-    """
-    api: paddle.MaxPool2D
-    op version: 9, 10, 11, 12
-    """
-    op = Net(kernel_size=5, padding='VALID')
-    op.eval()
-    # net, name, ver_list, delta=1e-6, rtol=1e-5
-    obj = APIOnnx(op, 'nn_MaxPool2D', [9, 10, 11, 12])
-    obj.set_input_data(
-        "input_data",
-        paddle.to_tensor(
-            randtool("float", -1, 1, [3, 1, 10, 10]).astype('float32')))
-    obj.run()
-
-
-def test_MaxPool2D_base_SAME():
-    """
-    api: paddle.MaxPool2D
-    op version: 9, 10, 11, 12
-    """
-    op = Net(kernel_size=5, padding='SAME')
-    op.eval()
-    # net, name, ver_list, delta=1e-6, rtol=1e-5
-    obj = APIOnnx(op, 'nn_MaxPool2D', [9, 10, 11, 12])
-    obj.set_input_data(
-        "input_data",
-        paddle.to_tensor(
-            randtool("float", -1, 1, [3, 1, 10, 10]).astype('float32')))
-    obj.run()
-
-
-def test_MaxPool2D_base_Padding_0():
-    """
-    api: paddle.MaxPool2D
-    op version: 9, 10, 11, 12
-    """
-    op = Net(kernel_size=5, padding=[[0, 0], [0, 0], [1, 2], [3, 4]])
-    op.eval()
-    # net, name, ver_list, delta=1e-6, rtol=1e-5
-    obj = APIOnnx(op, 'nn_MaxPool2D', [9, 10, 11, 12])
-    obj.set_input_data(
-        "input_data",
-        paddle.to_tensor(
-            randtool("float", -1, 1, [3, 1, 10, 10]).astype('float32')))
-    obj.run()
-
-
-def test_MaxPool2D_base_Padding_1():
-    """
-    api: paddle.MaxPool2D
-    op version: 9, 10, 11, 12
-    """
-    op = Net(kernel_size=5, padding=[1, 2, 3, 4])
-    op.eval()
-    # net, name, ver_list, delta=1e-6, rtol=1e-5
-    obj = APIOnnx(op, 'nn_MaxPool2D', [9, 10, 11, 12])
-    obj.set_input_data(
-        "input_data",
-        paddle.to_tensor(
-            randtool("float", -1, 1, [3, 1, 10, 10]).astype('float32')))
-    obj.run()
-
-
-def test_MaxPool2D_base_Padding_2():
-    """
-    api: paddle.MaxPool2D
-    op version: 9, 10, 11, 12
-    """
-    op = Net(kernel_size=5, padding=[1, 2])
-    op.eval()
-    # net, name, ver_list, delta=1e-6, rtol=1e-5
-    obj = APIOnnx(op, 'nn_MaxPool2D', [9, 10, 11, 12])
-    obj.set_input_data(
-        "input_data",
-        paddle.to_tensor(
-            randtool("float", -1, 1, [3, 1, 10, 10]).astype('float32')))
-    obj.run()
-
-
-def test_MaxPool2D_base_Padding_3():
-    """
-    api: paddle.MaxPool2D
-    op version: 9, 10, 11, 12
-    """
-    op = Net(kernel_size=20, padding=2)
-    op.eval()
-    # net, name, ver_list, delta=1e-6, rtol=1e-5
-    obj = APIOnnx(op, 'nn_MaxPool2D', [9, 10, 11, 12])
-    obj.set_input_data(
-        "input_data",
-        paddle.to_tensor(
-            randtool("float", -1, 1, [3, 1, 10, 10]).astype('float32')))
-    obj.run()
-
-
-def test_MaxPool2D_base_Padding_4():
-    """
-    api: paddle.MaxPool2D
-    op version: 9, 10, 11, 12
-    """
-    op = Net(kernel_size=5, padding=[1, 2, 3, 5])
-    op.eval()
-    # net, name, ver_list, delta=1e-6, rtol=1e-5
-    obj = APIOnnx(op, 'nn_MaxPool2D', [9, 10, 11, 12])
-    obj.set_input_data(
-        "input_data",
-        paddle.to_tensor(
-            randtool("float", -1, 1, [3, 1, 10, 10]).astype('float32')))
-    obj.run()
-
-
-# if __name__ == '__main__':
-#     test_MaxPool2D_base_Padding_4()
-#     test_MaxPool2D_base_Padding_3()
-#     test_MaxPool2D_base_Padding_2()
-#     test_MaxPool2D_base_Padding_1()
-#     test_MaxPool2D_base_Padding_0()
-#     test_MaxPool2D_base_SAME()
-#     test_MaxPool2D_base_VALID()
-#     test_MaxPool2D_base()
