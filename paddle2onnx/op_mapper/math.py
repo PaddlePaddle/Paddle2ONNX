6f8cc7c8b006dbb9760f154a5c72dd494f049c79
huangshenghui <huangshenghui@baidu.com>
2021-12-23 13:00:02 +0800

    update opset version

--- paddle2onnx/op_mapper/math.py
+++ paddle2onnx/op_mapper/math.py
@@ -679,7 +679,7 @@ class ReduceAll():
         'reduce_prod': 'ReduceProd'
     })
 class ReduceMean():
-    support_opset_version_range = (1, 12)
+    support_opset_version_range = (1, 15)
 
     @classmethod
     def opset_1(cls, graph, node, **kw):
