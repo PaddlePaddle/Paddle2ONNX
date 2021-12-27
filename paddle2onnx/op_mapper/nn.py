15f24dd833e6226bdc21f1b1f52c77df76ba4c1c
wjj19950828 <wjjisloser@163.com>
2021-12-27 11:15:09 +0800

    Add hardsigmoid testcase

--- paddle2onnx/op_mapper/nn.py
+++ paddle2onnx/op_mapper/nn.py
@@ -357,7 +357,7 @@ class Pool3D():
 
 @op_mapper('elu')
 class ELU():
-    support_opset_version_range = (1, 12)
+    support_opset_version_range = (7, 15)
 
     @classmethod
     def opset_1(cls, graph, node, **kw):
