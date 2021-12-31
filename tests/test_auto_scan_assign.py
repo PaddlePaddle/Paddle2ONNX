0218d4e13cbfcbea2420fed927640a29ab5b9dfc
huangshenghui <huangshenghui@baidu.com>
2021-12-31 17:50:33 +0800

    update

--- tests/test_auto_scan_assign.py
+++ tests/test_auto_scan_assign.py
@@ -43,9 +43,9 @@ class TestAssignConvert(OPConvertAutoScanTest):
         input_shape = draw(
             st.lists(
                 st.integers(
-                    min_value=4, max_value=10), min_size=2, max_size=2))
+                    min_value=4, max_value=8), min_size=2, max_size=5))
 
-        dtype = draw(st.sampled_from(["float32"]))
+        dtype = draw(st.sampled_from(["float32", "float64", "int32", "int64"]))
 
         config = {
             "op_names": ["assign"],
