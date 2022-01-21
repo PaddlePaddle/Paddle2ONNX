ec420145816111f7882339b5fcb58d859e9dfbbe
yeliang2258 <1047690002@qq.com>
2022-01-21 02:19:50 +0000

    update

--- paddle2onnx/convert.py
+++ paddle2onnx/convert.py
@@ -34,8 +34,7 @@ def export_onnx(paddle_graph,
     onnx_graph = ONNXGraph.build(paddle_graph, opset_version,
                                  operator_export_type, verbose,
                                  auto_update_opset)
-    onnx_graph = PassManager.run_pass(
-        onnx_graph, ['inplace_node_pass', 'remove_isolated_node_pass'])
+    onnx_graph = PassManager.run_pass(onnx_graph, ['inplace_node_pass'])
 
     onnx_proto = onnx_graph.export_proto(enable_onnx_checker)
 
