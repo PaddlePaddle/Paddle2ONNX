b79e9e8505949bdf743f5e71a199197db7707023
yeliang2258 <30516196+yeliang2258@users.noreply.github.com>
2022-01-29 23:20:06 +0800

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -16,6 +16,7 @@ cases=`find . -name "test*.py" | sort`
 ignore="test_expand_as.py \
         test_split.py \
         test_isnan.py \
+        test_has_nan.py \
         test_uniform.py"
 bug=0
 export PY_CMD=$1
