56c7439d3175a9548f6d5a961d5cec1d1c30e510
yeliang2258 <1047690002@qq.com>
2022-02-09 12:18:34 +0000

    add auto scan test

--- paddle2onnx/__init__.py
+++ paddle2onnx/__init__.py
@@ -1,3 +1,17 @@
+#   Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -7,17 +21,54 @@ import paddle2onnx.paddle2onnx_cpp2py_export as c_p2o
 from paddle2onnx import version
 import onnx
 
-def export(model_filename, params_filename="", save_file=None, opset_version=9, auto_upgrade_opset=True, verbose=True):
+
+def export(model_filename,
+           params_filename="",
+           save_file=None,
+           opset_version=9,
+           auto_upgrade_opset=True,
+           verbose=True):
     onnx_proto = onnx.ModelProto()
-    onnx_proto_str = c_p2o.export(model_filename, params_filename, opset_version, auto_upgrade_opset, verbose)
+    onnx_proto_str = c_p2o.export(model_filename, params_filename,
+                                  opset_version, auto_upgrade_opset, verbose)
     onnx_proto.ParseFromString(onnx_proto_str)
     if save_file is not None:
         with open(save_file, "wb") as f:
             f.write(onnx_proto.SerializeToString())
 
+
 def dygraph2onnx(layer, save_file, input_spec, opset_version=9, **configs):
     import os
     import paddle
-    dirname = os.path.split(save_file)[-1]
+    dirname = os.path.split(save_file)[0]
     paddle.jit.save(layer, os.path.join(dirname, "model"), input_spec)
-    export(os.path.join(dirname, 'model.pdmodel'), os.path.join(dirname, 'model.pdiparams'), save_file=save_file, opset_version=opset_version, auto_upgrade_opset=False)
+    auto_upgrade_opset = False
+    if 'auto_upgrade_opset' in configs:
+        if isinstance(configs['auto_upgrade_opset'], bool):
+            auto_upgrade_opset = configs['auto_upgrade_opset']
+        else:
+            raise TypeError(
+                "The auto_upgrade_opset should be 'bool', but received type is %s."
+                % type(configs['auto_upgrade_opset']))
+
+    get_op_list = False
+    if 'get_op_list' in configs:
+        if isinstance(configs['get_op_list'], bool):
+            get_op_list = configs['get_op_list']
+        else:
+            raise TypeError(
+                "The get_op_list should be 'bool', but received type is %s." %
+                type(configs['get_op_list']))
+
+    if get_op_list:
+        op_list = c_p2o.check_op(
+            os.path.join(dirname, 'model.pdmodel'),
+            os.path.join(dirname, 'model.pdiparams'))
+        return op_list
+
+    export(
+        os.path.join(dirname, 'model.pdmodel'),
+        os.path.join(dirname, 'model.pdiparams'),
+        save_file=save_file,
+        opset_version=opset_version,
+        auto_upgrade_opset=auto_upgrade_opset)
