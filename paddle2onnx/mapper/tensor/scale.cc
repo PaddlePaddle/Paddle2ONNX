081b0bf684616cecf676275bf9631254a991bd9c
yeliang2258 <1047690002@qq.com>
2022-02-17 05:12:54 +0000

    mv scale from nn to tensor

--- paddle2onnx/mapper/tensor/scale.cc
+++ paddle2onnx/mapper/tensor/scale.cc
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include "paddle2onnx/mapper/nn/scale.h"
+#include "paddle2onnx/mapper/tensor/scale.h"
 #include <vector>
 
 namespace paddle2onnx {
