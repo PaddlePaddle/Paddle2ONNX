85919acc47016beca30bd9a507e40f5f12d0e62c
huangshenghui <huangshenghui@baidu.com>
2022-02-19 09:31:35 +0800

    code stype

--- tests/test_auto_scan_interpolate_v1_ops.py
+++ tests/test_auto_scan_interpolate_v1_ops.py
@@ -41,7 +41,7 @@ op_set_map = {
 }
 
 
-class Net1(BaseNet):
+class Net2(BaseNet):
     """
     simple Net
     """
@@ -50,9 +50,6 @@ class Net1(BaseNet):
         """
         forward
         """
-        align_corners = self.config['align_corners']
-        align_mode = self.config['align_mode']
-
         scale = self.config['scale_factor']
         if self.config['is_scale_tensor'] and scale is not None:
             scale = paddle.to_tensor(scale, dtype=self.config['scale_dtype'])
@@ -61,6 +58,8 @@ class Net1(BaseNet):
         if self.config['is_size_tensor'] and out_shape is not None:
             out_shape = paddle.to_tensor(out_shape, self.config['size_dtype'])
 
+        align_corners = self.config['align_corners']
+        align_mode = self.config['align_mode']
         data_format = self.config['data_format']
         mode = self.config['mode']
 
@@ -75,7 +74,7 @@ class Net1(BaseNet):
         return x
 
 
-class TestInterpolateConvert1(OPConvertAutoScanTest):
+class TestInterpolateConvert2(OPConvertAutoScanTest):
     """
     api: paddle.nn.functional.interpolate
     OPset version: 9, 11, 15
@@ -160,7 +159,137 @@ class TestInterpolateConvert1(OPConvertAutoScanTest):
             "scale_dtype": scale_dtype,
         }
 
-        models = Net1(config)
+        models = Net2(config)
+
+        return (config, models)
+
+    def test(self):
+        self.run_and_statis(max_examples=80)
+
+
+class Net3(BaseNet):
+    """
+    simple Net
+    """
+
+    def forward(self, inputs):
+        """
+        forward
+        """
+        scale = None
+        out_shape = self.config['size']
+        mode = self.config['mode']
+
+        if mode == "LINEAR":
+            out_shape = [paddle.to_tensor(12, self.config['size_dtype'])]
+        elif mode in ["NEAREST", "BILINEAR"]:
+            out_shape = [paddle.to_tensor(12, self.config['size_dtype']), 14]
+        elif mode == "TRILINEAR":
+            out_shape = [
+                paddle.to_tensor(12, self.config['size_dtype']), 13, 14
+            ]
+
+        align_corners = self.config['align_corners']
+        align_mode = self.config['align_mode']
+        data_format = self.config['data_format']
+
+        x = paddle.fluid.layers.image_resize(
+            inputs,
+            out_shape=out_shape,
+            scale=scale,
+            resample=mode,
+            align_corners=align_corners,
+            align_mode=align_mode,
+            data_format=data_format)
+        return x
+
+
+class TestInterpolateConvert3(OPConvertAutoScanTest):
+    """
+    api: paddle.nn.functional.interpolate
+    OPset version: 9, 11, 15
+    """
+
+    def sample_convert_config(self, draw):
+        input_shape = draw(
+            st.lists(
+                st.integers(
+                    min_value=2, max_value=8), min_size=5, max_size=6))
+
+        dtype = draw(st.sampled_from(["float32"]))
+        # mode = draw(st.sampled_from(["LINEAR"]))
+        # mode = draw(st.sampled_from(["NEAREST"]))
+        # mode = draw(st.sampled_from(["BILINEAR"]))
+        # mode = draw(st.sampled_from(["TRILINEAR"]))
+        mode = draw(
+            st.sampled_from(["LINEAR", "NEAREST", "BILINEAR", "TRILINEAR"]))
+        align_corners = draw(st.booleans())
+        align_mode = draw(st.integers(min_value=0, max_value=1))
+        data_format = data_format_map[mode]
+        if data_format == "NCW":
+            input_shape = np.random.choice(input_shape, 3)
+            input_shape[0] = 1  # there is a bug when index > 1
+        elif data_format == "NCHW":
+            input_shape = np.random.choice(input_shape, 4)
+        else:
+            input_shape = np.random.choice(input_shape, 5)
+
+        size_dtype = draw(st.sampled_from(["int32"]))
+        scale_dtype = draw(st.sampled_from(["float32"]))
+
+        is_scale_tensor = False
+        is_size_tensor = False
+        if draw(st.booleans()):
+            size = None
+            # scale_factor should b even. eg [2, 4, 6, 8, 10]
+            is_scale_tensor = draw(st.booleans())
+            scale_factor = draw(st.integers(min_value=2, max_value=10))
+            scale_factor = scale_factor + 1 if scale_factor % 2 != 0 else scale_factor
+        else:
+            scale_factor = None
+            is_size_tensor = draw(st.booleans())
+            size1 = draw(st.integers(min_value=12, max_value=30))
+            size2 = draw(st.integers(min_value=12, max_value=30))
+            size3 = draw(st.integers(min_value=12, max_value=30))
+            if mode == 'LINEAR':
+                size = [size1]
+            if mode in ['NEAREST', 'BILINEAR']:
+                # NEAREST, size should be even
+                if mode == 'NEAREST':
+                    size1 = size1 + 1 if size1 % 2 != 0 else size1
+                    size2 = size2 + 1 if size2 % 2 != 0 else size2
+                size = [size1, size2]
+            if mode == "TRILINEAR":
+                size = [size1, size2, size3]
+
+        op_name = op_api_map[mode]
+        opset_version = op_set_map[mode]
+
+        if align_mode == 0 and mode in ["LINEAR", "BILINEAR", "TRILINEAR"]:
+            opset_version = [11, 12, 13, 14, 15]
+
+        if align_corners:
+            opset_version = [11, 12, 13, 14, 15]
+
+        config = {
+            "op_names": [op_name],
+            "test_data_shapes": [input_shape],
+            "test_data_types": [[dtype]],
+            "opset_version": opset_version,
+            "input_spec_shape": [],
+            "size": size,
+            "scale_factor": scale_factor,
+            "mode": mode,
+            "align_corners": align_corners,
+            "align_mode": align_mode,
+            "data_format": data_format,
+            "is_scale_tensor": is_scale_tensor,
+            "is_size_tensor": is_size_tensor,
+            "size_dtype": size_dtype,
+            "scale_dtype": scale_dtype,
+        }
+
+        models = Net3(config)
 
         return (config, models)
 
