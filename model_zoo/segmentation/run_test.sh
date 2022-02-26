6ab57c87d6eeb2f3483345242e4b80b2a1dea4de
neonhuang <1174748008@qq.com>
2022-02-26 03:36:22 +0000

    fix run_test.sh

--- model_zoo/segmentation/run_test.sh
+++ model_zoo/segmentation/run_test.sh
@@ -1,12 +1,45 @@
-# model=bisenet
-# model=danet
-# model=deeplabv3
-# model=deeplabv3p
-model=fcn
+PaddleSegPATH=/home/x2paddle/huangshenghui/PaddleSeg
+Paddle2ONNXPATH=/home/x2paddle/huangshenghui/Paddle2ONNX
+cd $PaddleSegPATH
+mkdir -p inference/
 
+model=bisenet
+config_file=bisenet_cityscapes_1024x1024_160k.yml
+model_url=https://bj.bcebos.com/paddleseg/dygraph/cityscapes/bisenet_cityscapes_1024x1024_160k/model.pdparams
+
+# model=danet
+# config_file=danet_resnet50_os8_cityscapes_1024x512_80k.yml
+# model_url=https://bj.bcebos.com/paddleseg/dygraph/cityscapes/danet_resnet50_os8_cityscapes_1024x512_80k/model.pdparams
+
+# model=deeplabv3
+# config_file=deeplabv3_resnet50_os8_cityscapes_1024x512_80k.yml
+# model_url=https://bj.bcebos.com/paddleseg/dygraph/cityscapes/deeplabv3_resnet50_os8_cityscapes_1024x512_80k/model.pdparams
+
+# model=deeplabv3p
+# config_file=deeplabv3p_resnet50_os8_cityscapes_1024x512_80k.yml
+# model_url=https://bj.bcebos.com/paddleseg/dygraph/cityscapes/deeplabv3p_resnet50_os8_cityscapes_1024x512_80k/model.pdparams
+
+# model=fcn
+# config_file=fcn_hrnetw18_cityscapes_1024x512_80k.yml
+# model_url=https://bj.bcebos.com/paddleseg/dygraph/cityscapes/fcn_hrnetw18_cityscapes_1024x512_80k/model.pdparams
+
+rm -rf inference/$model
+mkdir -p inference/$model
+cd inference/$model
+wget $model_url
+cd ../..
+
+python3.7 export.py \
+       --config configs/$model/$config_file \
+       --model_path inference/$model/model.pdparams\
+       --save_dir inference/$model
+
+rm -rf inference/$model/model.pdparams
+
+cd $Paddle2ONNXPATH/model_zoo/segmentation
 mkdir inference
 rm -rf inference/$model
-cp -r /home/x2paddle/huangshenghui/PaddleSeg/inference/$model ./inference
+cp -r $PaddleSegPATH/inference/$model ./inference
 
 rm -rf cityscapes_demo.png
 wget https://paddleseg.bj.bcebos.com/dygraph/demo/cityscapes_demo.png
