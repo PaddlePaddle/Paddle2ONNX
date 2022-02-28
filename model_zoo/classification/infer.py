fed419cfe8d87e74ff3cc2c433becaf5c8c3e5a2
Jason <jjjkaixin@baidu.com>
2022-02-28 15:02:24 +0800

    modify classification model zoo

--- model_zoo/classification/infer.py
+++ model_zoo/classification/infer.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,89 +12,117 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import sys
-
-sys.path.insert(0, ".")
 import argparse
 import numpy as np
-from PIL import Image
-
-import paddle
-from onnxruntime import InferenceSession
-
-# 从模型代码中导入模型
-from utils.presets import ClassificationPresetEval
-from utils.presets import Topk
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--model_path', type=str, help="paddle model filename")
-    parser.add_argument('--onnx_path', type=str, help="onnx model filename")
+    parser.add_argument(
+        '--model',
+        type=str,
+        help="while use_paddle_predict, this means directory path of paddle model. Other wise, this means path of onnx model file."
+    )
     parser.add_argument('--image_path', type=str, help="image filename")
+    parser.add_argument(
+        '--use_paddle_predict',
+        type=bool,
+        default=False,
+        help="If use paddlepaddle to predict, otherwise use onnxruntime to predict."
+    )
     parser.add_argument('--crop_size', default=224, help='crop_szie')
     parser.add_argument('--resize_size', default=256, help='resize_size')
     return parser.parse_args()
 
 
-def preprocess(img_path):
-    # define transforms
-    eval_transforms = ClassificationPresetEval(
-        crop_size=FLAGS.crop_size, resize_size=FLAGS.resize_size)
-    # 准备输入
-    with open(img_path, 'rb') as f:
-        img = Image.open(f).convert('RGB')
+def preprocess(image_path):
+    """ Preprocess input image file
+    Args:
+        image_path(str): Path of input image file
 
-    data = eval_transforms(img)
-    data = np.expand_dims(data, axis=0)
+
+    Returns:
+        preprocessed data(np.ndarray): Shape of [N, C, H, W]
+    """
+    import cv2
+
+    def resize_by_short(im, resize_size):
+        short_size = min(im.shape[0], im.shape[1])
+        scale = FLAGS.resize_size / short_size
+        new_w = int(round(im.shape[1] * scale))
+        new_h = int(round(im.shape[0] * scale))
+        return cv2.resize(im, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
+
+    def center_crop(im, crop_size):
+        h, w, c = im.shape
+        w_start = (w - crop_size) // 2
+        h_start = (h - crop_size) // 2
+        w_end = w_start + crop_size
+        h_end = h_start + crop_size
+        return im[h_start:h_end, w_start:w_end, :]
+
+    def normalize(im, mean, std):
+        print("-------", im.shape)
+        im = im.astype("float32") / 255.0
+        print("+++++++", im.shape)
+        mean = np.array(mean).reshape((1, 1, 3)).astype("float32")
+        std = np.array(std).reshape((1, 1, 3)).astype("float32")
+        print("NORMRR", im.shape)
+        return (im - mean) / std
+
+    # resize the short edge to `resize_size`
+    im = cv2.imread(image_path)
+    resized_im = resize_by_short(im, FLAGS.resize_size)
+    print("000001", resized_im.shape)
+
+    # crop from center
+    croped_im = center_crop(resized_im, FLAGS.crop_size)
+    print("000002", croped_im.shape)
+
+    # normalize
+    normalized_im = normalize(croped_im, [0.485, 0.456, 0.406],
+                              [0.229, 0.224, 0.225])
+    print("000003", normalized_im.shape)
+
+    # transpose to NCHW
+    data = np.expand_dims(normalized_im, axis=0)
+    data = np.transpose(data, (0, 3, 1, 2))
+
     return data
 
 
-def postprocess(results):
-    topk = Topk(5, "./utils/imagenet1k_label_list.txt")
-    data = topk(results)
-    # class_id = results.argmax()
-    # prob = results[0][class_id]
-    return data
+def postprocess(result, topk=5):
+    # choose topk index and score
+    scores = result.flatten()
+    topk_indices = np.argsort(-1 * scores)[:topk]
+    topk_scores = scores[topk_indices]
+    print("TopK Indices: ", topk_indices)
+    print("TopK Scores: ", topk_scores)
 
 
-def print_detail(batch_results):
-    for number, result_dict in enumerate(batch_results):
-        clas_ids = result_dict["class_ids"]
-        scores_str = "[{}]".format(", ".join("{:.2f}".format(r)
-                                             for r in result_dict["scores"]))
-        label_names = result_dict["label_names"]
-        print("class id(s): {}, score(s): {}, label_name(s): {}".format(
-            clas_ids, scores_str, label_names))
+def paddle_predict(paddle_model_dir, image_path):
+    import paddle
+    import os
+    model = paddle.jit.load(os.path.join(paddle_model_dir, "inference"))
 
-
-def paddle_predict(model_path, image_path):
-    model = paddle.jit.load(model_path)
-    model.eval()
     data = preprocess(image_path)
-    results = model(data).numpy()
-    results = postprocess(results)
-    return results
+    result = model(paddle.to_tensor(data)).numpy()
+    postprocess(result)
 
 
-def onnx_predict(onnx_path, image_path):
-    sess = InferenceSession(onnx_path)
+def onnx_predict(onnx_model_path, image_path):
+    import onnxruntime
+    sess = onnxruntime.InferenceSession(onnx_model_path)
     data = preprocess(image_path)
-    results = sess.run(None, input_feed={sess.get_inputs()[0].name: data})[0]
-    results = postprocess(results)
-    return results
+    result, = sess.run(None, {"inputs": data})
+    print("00000", result.shape)
+    postprocess(result)
 
 
 if __name__ == '__main__':
     FLAGS = parse_args()
 
-    data = paddle_predict(FLAGS.model_path, FLAGS.image_path)
-    print("Paddle inference top-5 results: ")
-    print_detail(data)
-    # print(f"paddle result: class_id: {class_id}, prob: {prob}")
-
-    data = onnx_predict(FLAGS.onnx_path, FLAGS.image_path)
-    print("ONNXRuntime top-5  results: ")
-    print_detail(data)
-    # print(f"onxx result: class_id: {class_id}, prob: {prob}")
+    if FLAGS.use_paddle_predict:
+        paddle_predict(FLAGS.model, FLAGS.image_path)
+    else:
+        onnx_predict(FLAGS.model, FLAGS.image_path)
