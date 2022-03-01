9053def5f4d8c0f307854980b601f0ef2e7f0925
neonhuang <87560459+neonhuang@users.noreply.github.com>
2022-03-01 16:19:17 +0800

    fix segmention model zoo (#586)
    * fix a bug for squeeze and version
    
    * update
    
    * update infer.py
    
    * infer.py and readme
    
    Co-authored-by: Jason <jiangjiajun@baidu.com>

--- model_zoo/segmentation/infer.py
+++ model_zoo/segmentation/infer.py
@@ -14,20 +14,10 @@
 
 import argparse
 import os
-import sys
-
-LOCAL_PATH = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(LOCAL_PATH, '..', '..'))
-
+import cv2
 import numpy as np
-
-import paddleseg.transforms as T
-from paddleseg.cvlibs import manager
-from paddleseg.utils import logger, get_image_list
-from paddleseg.utils.visualize import get_pseudo_color_map
-
-import onnxruntime as rt
 import paddle
+import onnxruntime as rt
 
 
 def parse_args():
@@ -35,21 +25,20 @@ def parse_args():
     parser.add_argument(
         '--model_path',
         dest='model_path',
-        help='The path of the paddle pdmodel',
         type=str,
-        required=True)
-    parser.add_argument(
-        '--onnx_path',
-        dest='onnx_path',
-        help='file of onnx of model.',
-        type=str,
-        required=True)
+        help="while use_paddle_predict, this means directory path of paddle model. Other wise, this means path of "
+        "onnx model file.")
     parser.add_argument(
         '--image_path',
         dest='image_path',
-        help='The directory or path or file list of the images to be predicted.',
         type=str,
-        required=True)
+        help='The directory or path or file list of the images to be predicted.')
+    parser.add_argument(
+        '--use_paddle_predict',
+        type=bool,
+        default=False,
+        help="If use paddlepaddle to predict, otherwise use onnxruntime to predict."
+    )
     parser.add_argument(
         '--save_dir',
         dest='save_dir',
@@ -83,19 +72,25 @@ def onnx_predict(onnx_path, imgs_path):
     return results
 
 
-def preprocess(img):
-    t_list = [{'type': 'Normalize'}]
-    com = manager.TRANSFORMS
-    transforms = []
-    for t in t_list:
-        transforms.append(com[t.pop('type')](**t))
-    transforms = T.Compose(transforms)
-    data = np.array([transforms(img)[0]])
+def preprocess(image_path):
+    def normalize(im, mean, std):
+        mean = np.array(mean)[np.newaxis, np.newaxis, :]
+        std = np.array(std)[np.newaxis, np.newaxis, :]
+        im = im.astype(np.float32, copy=False) / 255.0
+        im -= mean
+        im /= std
+        im = np.transpose(im, (2, 0, 1))
+        return im
+
+    im = cv2.imread(image_path).astype('float32')
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    data = normalize(im, [0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    data = np.expand_dims(data, axis=0)
     return data
 
 
 def postprocess(results):
-    if args.with_argmax:
+    if FLAGS.with_argmax:
         results = np.argmax(results, axis=1)
     return results
 
@@ -108,42 +103,47 @@ def save_imgs(results, imgs_path, prefix=None):
         if prefix is not None and isinstance(prefix, str):
             basename = prefix + "_" + basename
         basename = f'{basename}.png'
-        result.save(os.path.join(args.save_dir, basename))
+        result.save(os.path.join(FLAGS.save_dir, basename))
 
 
-def main(args):
-    imgs_path = args.image_path
-    if not os.path.exists(args.save_dir):
-        os.makedirs(args.save_dir)
+def get_pseudo_color_map(pred, color_map=None):
+    from PIL import Image as PILImage
+    pred_mask = PILImage.fromarray(pred.astype(np.uint8), mode='P')
+    if color_map is None:
+        color_map = get_color_map_list(256)
+    pred_mask.putpalette(color_map)
+    return pred_mask
 
-    paddle_result = paddle_predict(args.model_path, imgs_path)
-    save_imgs(paddle_result, imgs_path, "paddle")
 
-    onnx_result = onnx_predict(args.onnx_path, imgs_path)
-    save_imgs(onnx_result, imgs_path, "onnx")
+def get_color_map_list(num_classes, custom_color=None):
+    num_classes += 1
+    color_map = num_classes * [0, 0, 0]
+    for i in range(0, num_classes):
+        j = 0
+        lab = i
+        while lab:
+            color_map[i * 3] |= (((lab >> 0) & 1) << (7 - j))
+            color_map[i * 3 + 1] |= (((lab >> 1) & 1) << (7 - j))
+            color_map[i * 3 + 2] |= (((lab >> 2) & 1) << (7 - j))
+            j += 1
+            lab >>= 3
+    color_map = color_map[3:]
 
-    diff = onnx_result - paddle_result
-    max_abs_diff = np.fabs(diff).max()
-    if max_abs_diff < 1e-05:
-        print(
-            "The difference of results between ONNXRuntime and Paddle looks good!"
-        )
-    else:
-        relative_diff = max_abs_diff / np.fabs(paddle_result).max()
-        if relative_diff < 1e-05:
-            print(
-                "The difference of results between ONNXRuntime and Paddle looks good!"
-            )
-        else:
-            print(
-                "The difference of results between ONNXRuntime and Paddle looks bad!"
-            )
-        print('relative_diff: ', relative_diff)
-    print('max_abs_diff: ', max_abs_diff)
-
-    logger.info("Finish")
+    if custom_color:
+        color_map[:len(custom_color)] = custom_color
+    return color_map
 
 
 if __name__ == '__main__':
-    args = parse_args()
-    main(args)
+    FLAGS = parse_args()
+    imgs_path = FLAGS.image_path
+    if not os.path.exists(FLAGS.save_dir):
+        os.makedirs(FLAGS.save_dir)
+
+    if FLAGS.use_paddle_predict:
+        paddle_result = paddle_predict(FLAGS.model_path, imgs_path)
+        save_imgs(paddle_result, imgs_path, "paddle")
+    else:
+        onnx_result = onnx_predict(FLAGS.model_path, imgs_path)
+        save_imgs(onnx_result, imgs_path, "onnx")
+    print("Finish")
--- model_zoo/segmentation/readme.md
+++ model_zoo/segmentation/readme.md
@@ -6,75 +6,39 @@
 |BiSeNet|[bisenet_cityscapes_1024x1024_160k.yml](https://github.com/PaddlePaddle/PaddleSeg/blob/release/2.4/configs/bisenet/bisenet_cityscapes_1024x1024_160k.yml)|9.4M|[推理模型]() / [ONNX模型](model.onnx)| 使用CityScape数据作为训练数据，19个分类，包括车、路、人等等 |
 |DANet|[danet_resnet50_os8_cityscapes_1024x512_80k.yml](https://github.com/PaddlePaddle/PaddleSeg/blob/release/2.4/configs/danet/danet_resnet50_os8_cityscapes_1024x512_80k.yml)|190.1M|[推理模型]() / [ONNX模型](model.onnx)| 使用CityScape数据作为训练数据，19个分类，包括车、路、人等等 |
 |DeepLabv3|[deeplabv3_resnet50_os8_cityscapes_1024x512_80k.yml](https://github.com/PaddlePaddle/PaddleSeg/blob/release/2.4/configs/deeplabv3/deeplabv3_resnet50_os8_cityscapes_1024x512_80k.yml)|156.6M|[推理模型]() / [ONNX模型](model.onnx)| 使用CityScape数据作为训练数据，19个分类，包括车、路、人等等 |
-|Deeplabv3P|[deeplabv3p_resnet50_os8_cityscapes_1024x512_80k.yml](https://github.com/PaddlePaddle/PaddleSeg/blob/release/2.4/configs/deeplabv3p/deeplabv3p_resnet50_os8_cityscapes_1024x512_80k.yml)|107.2M|[推理模型]() / [ONNX模型](model.onnx)| 使用CityScape数据作为训练数据，19个分类，包括车、路、人等等 |
-|FCN|[fcn_hrnetw18_cityscapes_1024x512_80k.yml](https://github.com/PaddlePaddle/PaddleSeg/blob/release/2.4/configs/fcn/fcn_hrnetw18_cityscapes_1024x512_80k.yml)|39M|[推理模型]() / [ONNX模型](model.onnx)| 使用CityScape数据作为训练数据，19个分类，包括车、路、人等等 |
 
 
-# 模型推理预测
+# ONNX模型推理示例
 
-- 环境依赖
-    - paddlepaddle >= 2.0.2
-    - paddleseg >= 2.4
-    - paddle2onnx >= 0.9
-    - onnxruntime >= 1.9.0
-
-- 下载模型
-
-以BiSeNet为例：
-
-在[图像分割模型库](#图像分割模型库)，下载BiSeNet的推理模型和ONNX模型
+各模型的推理前后处理参考本目录下的infer.py，以BiseNet为例，如下命令即可得到推理结果
 
 ```bash
-wget -nc  -P ./inference https://paddleocr.bj.bcebos.com/paddle2onnx/models/pdmodel/bisenet.tar
-cd ./inference && tar xf bisenet.tar && cd ..
-
-wget -nc  -P ./inference https://paddleocr.bj.bcebos.com/paddle2onnx/models/onnx/bisenet.tar
-cd ./inference && tar xf bisenet.tar && cd ..
-```
-
-其中ONNX模型，也可以通过使用 Paddle2ONNX 将BiseNet的推理模型转换为ONNX格式，执行如下命令即可：
-
-```bash
-paddle2onnx --model_dir ./inference/bisenet \
---model_filename model.pdmodel \
---params_filename model.pdiparams \
---save_file ./inference/bisenet/model.onnx \
---opset_version 11 \
---input_shape_dict="{'x':[-1,3,-1,-1]}" \
---enable_onnx_checker True
-```
-
-执行完毕后，ONNX 模型会被分别保存在 `./inference/bisenet/`路径下
-
-- 推理预测
-
-ONNX模型测试步骤如下：
-
-- Step1：初始化`ONNXRuntime`库并配置相应参数, 并进行预测
-- Step2：`ONNXRuntime`预测结果和`Paddle Inference`预测结果对比
-
-下载cityscapes验证集中的一张[图片](https://paddleseg.bj.bcebos.com/dygraph/demo/cityscapes_demo.png)
-
-```bash
+# 安装onnxruntime
+pip3 install onnxruntime
+# 下载测试图
 wget https://paddleseg.bj.bcebos.com/dygraph/demo/cityscapes_demo.png
-```
 
-在本目录下，我们提供了`infer.py`脚本进行预测，执行如下命令即可：
+# 下载BiseNet模型
+wget https://bj.bcebos.com/paddle2onnx/model_zoo/bisenet.onnx
 
-```bash
-python3.7 infer.py \
-    --model_path ./inference/bisenet/model \
-    --onnx_path ./inference/bisenet/model.onnx \
+python3 infer.py \
+    --model_path bisenet.onnx \
     --image_path ./cityscapes_demo.png
 ```
 
-执行命令后在终端会打印出预测的识别信息，并在 ./outputs/ 下保存可视化结果。
+你也可以使用Paddle框架进行推理验证
 
-Paddle Inference 执行效果：
+```bash
+wget https://bj.bcebos.com/paddle2onnx/model_zoo/bisenet.tar.gz
+tar xvf bisenet.tar.gz
 
-<div align="center">
-    <img src="./images/paddle_cityscapes_demo.png" width=800">
-</div>
+python3 infer.py \
+    --model_path ./bisenet/model \
+    --image_path ./cityscapes_demo.png \
+    --use_paddle_predict True
+```
+
+执行命令后，在 ./outputs/ 下保存可视化结果。
 
 ONNXRuntime 执行效果：
 
@@ -82,13 +46,8 @@ ONNXRuntime 执行效果：
     <img src="./images/onnx_cityscapes_demo.png" width=800">
 </div>
 
-在终端输出结果如下。
+Paddle Inference 执行效果：
 
-```
-sess input/output name :  x argmax_0.tmp_0
-The difference of results between ONNXRuntime and Paddle looks good!
-max_abs_diff:  0.0
-
-```
-
-max_abs_diff为 0.0 表示`Paddle Inference`预测结果与`ONNXRuntime`引擎的结果完全一致。
+<div align="center">
+    <img src="./images/paddle_cityscapes_demo.png" width=800">
+</div>
