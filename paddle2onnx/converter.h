66548e6dd2d3a301cf587705178885c7608300e7
heliqi <1101791222@qq.com>
2022-03-16 07:58:09 +0000

    modify opset_version default

--- paddle2onnx/converter.h
+++ paddle2onnx/converter.h
@@ -28,14 +28,14 @@ namespace paddle2onnx {
 
 PADDLE2ONNX_DECL bool IsExportable(
     const std::string& model, const std::string& params,
-    bool from_memory_buffer = false, int32_t opset_version = 15,
+    bool from_memory_buffer = false, int32_t opset_version = 11,
     bool auto_upgrade_opset = true, bool verbose = false,
     bool enable_onnx_checker = true, bool enable_experimental_op = false,
     bool enable_optimize = true);
 
 PADDLE2ONNX_DECL bool Export(
     const std::string& model, const std::string& params, std::string* out,
-    bool from_memory_buffer = false, int32_t opset_version = 15,
+    bool from_memory_buffer = false, int32_t opset_version = 11,
     bool auto_upgrade_opset = true, bool verbose = false,
     bool enable_onnx_checker = true, bool enable_experimental_op = false,
     bool enable_optimize = true);
