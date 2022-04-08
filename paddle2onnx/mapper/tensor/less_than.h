820c4cb7a3307bef2ca861102ea52ba101201103
jiangjiajun <jiangjiajun@baidu.com>
2022-04-08 07:33:15 +0000

    update some op level

--- paddle2onnx/mapper/tensor/less_than.h
+++ paddle2onnx/mapper/tensor/less_than.h
@@ -21,9 +21,7 @@ class LessThanMapper : public Mapper {
  public:
   LessThanMapper(const PaddleParser& p, OnnxHelper* helper, int64_t block_id,
                  int64_t op_id)
-      : Mapper(p, helper, block_id, op_id) {
-    MarkAsExperimentalOp();
-  }
+      : Mapper(p, helper, block_id, op_id) {}
   void Opset7();
 };
 
