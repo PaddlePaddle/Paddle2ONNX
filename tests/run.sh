ea369df10748305cbd3f1bd6f18438f57049be73
neonhuang <87560459+neonhuang@users.noreply.github.com>
2022-05-04 11:59:37 +0800

    add argsort and dist  op(C++) (#703)
    * add argsort c++ op
    
    * fix dist test case
    
    * update run.sh
    
    * Update run.sh
    
    * fix some code logic
    
    * code styles
    
    * fix argsort
    
    * fix argsort op
    
    * fix dist op
    
    * fix run.sh
    
    Co-authored-by: Jason <jiangjiajun@baidu.com>
    Co-authored-by: Jason <928090362@qq.com>

--- tests/run.sh
+++ tests/run.sh
@@ -38,8 +38,72 @@ done
 
 export ENABLE_DEV=ON
 dev_tests=("test_auto_scan_conv2d.py \
-           test_auto_scan_log.py")
+            test_auto_scan_unary_ops.py \
+            test_auto_scan_assign.py \
+            test_auto_scan_batch_norm.py \
+            test_auto_scan_interpolate_ops.py \
+            test_auto_scan_bmm.py \
+            test_auto_scan_cast.py \
+            test_auto_scan_clip.py \
+            test_auto_scan_concat.py \
+            test_auto_scan_conv2d.py \
+            test_auto_scan_cumsum.py \
+            test_auto_scan_conv2d.py \
+            test_auto_scan_dropout.py \
+            test_auto_scan_elementwise_ops.py \
+            test_auto_scan_elu.py \
+            test_auto_scan_expand_v2.py \
+            test_auto_scan_fill_constant.py \
+            test_auto_scan_fill_like.py \
+            test_auto_scan_flatten.py \
+            test_auto_scan_gather.py \
+            test_auto_scan_gaussian_random.py \
+            test_auto_scan_gelu.py \
+            test_auto_scan_hardsigmoid.py \
+            test_auto_scan_hardswish.py \
+            test_auto_scan_layer_norm.py \
+            test_auto_scan_leakyrelu.py \
+            test_auto_scan_logsoftmax.py \
+            test_auto_scan_logsigmoid.py \
+            test_auto_scan_logsumexp.py \
+            test_auto_scan_lookup_table_v2.py \
+            test_auto_scan_matmul.py \
+            test_auto_scan_matmul_v2.py \
+            test_auto_scan_mean.py \
+            test_auto_scan_meshgrid.py \
+            test_auto_scan_logical_ops.py \
+            test_auto_scan_pad3d.py \
+            test_auto_scan_prelu.py \
+            test_auto_scan_range.py \
+            test_auto_scan_reducemean_ops.py \
+            test_auto_scan_reshape.py \
+            test_auto_scan_rnn.py \
+            test_auto_scan_scale.py \
+            test_auto_scan_shape.py \
+            test_auto_scan_size.py \
+            test_auto_scan_slice.py \
+            test_auto_scan_softmax.py \
+            test_auto_scan_shrink_ops.py \
+            test_auto_scan_split.py \
+            test_auto_scan_squeeze2.py \
+            test_auto_scan_stack.py \
+            test_auto_scan_strided_slice.py \
+            test_auto_scan_sum.py \
+            test_auto_scan_shrink_ops.py \
+            test_auto_scan_thresholded_relu.py \
+            test_auto_scan_tile.py \
+            test_auto_scan_top_k_v2.py \
+            test_auto_scan_transpose.py \
+            test_auto_scan_unsqueeze2.py \
+            test_auto_scan_where.py \
+            test_auto_scan_yolo_box.py \
+            test_auto_scan_dist.py \
+            test_auto_scan_yolo_box.py \
+            test_auto_scan_argsort.py \
+            test_auto_scan_expand_as.py \
+            test_auto_scan_log.py")
 echo "=============dev test=========" >>result.txt
+echo "=============dev test========="
 for file in ${dev_tests}
 do
     echo ${file}
