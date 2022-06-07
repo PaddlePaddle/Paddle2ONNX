cf3bbd22bceb9fc827a303b1cdd86410d5753b13
yeliang2258 <1047690002@qq.com>
2022-06-07 06:51:00 +0000

    add one_hot_v2 support

--- tests/run.sh
+++ tests/run.sh
@@ -32,7 +32,6 @@ ignore="test_expand_as.py \
         test_auto_scan_masked_select.py \
         test_auto_scan_mv.py \
         test_auto_scan_norm.py \
-        test_auto_scan_one_hot_v2.py \
         test_auto_scan_pad2d.py \
         test_auto_scan_pixel_shuffle.py \
         test_auto_scan_p_norm.py \
