d6906d89a230ce37b96850c1141363ec2023898c
yeliang2258 <1047690002@qq.com>
2022-06-29 04:44:55 +0000

    fix round type attr in quantize_linear

--- paddle2onnx/mapper/quantize/quantize_linear.h
+++ paddle2onnx/mapper/quantize/quantize_linear.h
@@ -36,8 +36,8 @@ class QuantizeLinearMapper : public Mapper {
   void Opset10();
 
  private:
-  int64_t round_type_ = -1;  // 0: rounding to nearest ties to even. 1: rounding
-                             // to nearest ties away from zero.
+  int64_t round_type_ = 0;  // 0: rounding to nearest ties to even. 1: rounding
+                            // to nearest ties away from zero.
   int64_t quant_axis_ = 1;
   int64_t bit_length_ = 8;
 };
