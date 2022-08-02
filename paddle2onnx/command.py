f4ca52de2def95b6a2e9134c934cc7f4b69e5c44
yeliang2258 <1047690002@qq.com>
2022-08-02 07:11:04 +0000

    switch enable_dev_version to True

--- paddle2onnx/command.py
+++ paddle2onnx/command.py
@@ -74,7 +74,7 @@ def arg_parser():
     parser.add_argument(
         "--enable_dev_version",
         type=ast.literal_eval,
-        default=False,
+        default=True,
         help="whether to use new version of Paddle2ONNX which is under developing, default False"
     )
     parser.add_argument(
