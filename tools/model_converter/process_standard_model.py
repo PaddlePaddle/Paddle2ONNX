0883f2966c3c8a01a447f82d23648562364f0f87
yeliang2258 <1047690002@qq.com>
2023-03-21 12:39:29 +0000

    update code

--- tools/model_converter/process_standard_model.py
+++ tools/model_converter/process_standard_model.py
@@ -220,8 +220,12 @@ class StandardModel(object):
         index = 0
         while index >= 0:
             operator_node = self.operator_node(index)
-            if len(operator_node.output) > 0:
+            if len(
+                    operator_node.output
+            ) > 0 and operator_node.operator_type not in ["feed", "fetch"]:
                 index = -1
+            else:
+                index += 1
         return operator_node.output
 
     def print_the_first_attribute(self):
@@ -304,36 +308,36 @@ if __name__ == '__main__':
     args = parse_arguments()
     paddle.set_device("cpu")
     model = StandardModel(args.standard_model)
-    print("-" * 10 + " Test NO 2 " + "-" * 10)
-    model.print_all_tensors()
-    print("-" * 10 + " Test NO 3 " + "-" * 10)
-    print(model.print_the_first_tensor())
-    print("-" * 10 + " Test NO 4 " + "-" * 10)
-    print(model.model())
-    print("-" * 10 + " Test NO 5 " + "-" * 10)
-    print(model.model().contributors)
-    print("-" * 10 + " Test NO 6 " + "-" * 10)
-    print(model.graph())
-    print("-" * 10 + " Test NO 7 " + "-" * 10)
-    print(model.operator_node(1))
+    # print("-" * 10 + " Test NO 2 " + "-" * 10)
+    # model.print_all_tensors()
+    # print("-" * 10 + " Test NO 3 " + "-" * 10)
+    # print(model.print_the_first_tensor())
+    # print("-" * 10 + " Test NO 4 " + "-" * 10)
+    # print(model.model())
+    # print("-" * 10 + " Test NO 5 " + "-" * 10)
+    # print(model.model().contributors)
+    # print("-" * 10 + " Test NO 6 " + "-" * 10)
+    # print(model.graph())
+    # print("-" * 10 + " Test NO 7 " + "-" * 10)
+    # print(model.operator_node(1))
     print("-" * 10 + " Test NO 8 " + "-" * 10)
     print(model.print_the_first_variable_type())
-    print("-" * 10 + " Test NO 9 " + "-" * 10)
-    print(model.print_the_first_attribute())
-    print("-" * 10 + " Test NO 10 " + "-" * 10)
-    for _, val in model.print_the_first_variable_type().items():
-        for variable_type in val.variable_type:
-            print(variable_type.data_type)
-    print("-" * 10 + " Test NO 11 " + "-" * 10)
-    print(model.tensor_str(0))
-    print("-" * 10 + " Test NO 12 " + "-" * 10)
-    for _, val in model.print_the_first_variable_type().items():
-        for variable_type in val.variable_type:
-            print(variable_type.tensor.shape)
-    print("-" * 10 + " Test NO 13 " + "-" * 10)
-    for _, val in model.print_the_first_variable_type().items():
-        for variable_type in val.variable_type:
-            print(variable_type.tensor.shape.dim)
+    # print("-" * 10 + " Test NO 9 " + "-" * 10)
+    # print(model.print_the_first_attribute())
+    # print("-" * 10 + " Test NO 10 " + "-" * 10)
+    # for _, val in model.print_the_first_variable_type().items():
+    #     for variable_type in val.variable_type:
+    #         print(variable_type.data_type)
+    # print("-" * 10 + " Test NO 11 " + "-" * 10)
+    # print(model.tensor_str(0))
+    # print("-" * 10 + " Test NO 12 " + "-" * 10)
+    # for _, val in model.print_the_first_variable_type().items():
+    #     for variable_type in val.variable_type:
+    #         print(variable_type.tensor.shape)
+    # print("-" * 10 + " Test NO 13 " + "-" * 10)
+    # for _, val in model.print_the_first_variable_type().items():
+    #     for variable_type in val.variable_type:
+    #         print(variable_type.tensor.shape.dim)
 
     if args.save_dir is None:
         args.save_dir = "paddle_model"
