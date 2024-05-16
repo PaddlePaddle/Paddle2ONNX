b8c13413f5eb67150e2cffba2dd1f0c29c7d6927
Zheng-Bicheng <zheng_bicheng@outlook.com>
2024-05-16 17:45:05 +0800

    update test_auto_scan_reduce_ops

--- tests/test_auto_scan_reduce_ops.py
+++ tests/test_auto_scan_reduce_ops.py
@@ -21,16 +21,16 @@ import paddle
 import random
 
 op_api_map = {
-    "reduce_max": paddle.max,
+    # "reduce_max": paddle.max,
     "reduce_min": paddle.min,
-    "reduce_mean": paddle.mean,
-    "reduce_sum": paddle.sum,
-    "reduce_prod": paddle.prod,
+    # "reduce_mean": paddle.mean,
+    # "reduce_sum": paddle.sum,
+    # "reduce_prod": paddle.prod,
 }
 
 opset_version_map = {
-    "reduce_max": [11, 12, 13, 18],
-    "reduce_min": [11, 12, 13, 18],
+    "reduce_max": [12, 13, 18],
+    "reduce_min": [12, 13, 18],
     "reduce_mean": [11, 13, 18],
     "reduce_sum": [13, 18],
     # "reduce_prod": [11, 18],
