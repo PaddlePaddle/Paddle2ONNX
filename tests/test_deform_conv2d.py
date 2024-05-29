0c0208e58923fc46aa424442d7084f20cfd0e1be
xiaoyewww <641311428@qq.com>
2024-05-29 07:06:02 +0000

    Add deform conv2d op

--- tests/test_deform_conv2d.py
+++ tests/test_deform_conv2d.py
@@ -27,15 +27,15 @@ def test_deform_conv2d():
     # [TODO] onnxruntime does not fully support DeformConv, by referring to 
     # https://github.com/onnx/onnx/issues/5451#issuecomment-1658439524
     
-    # op = Net()
-    # op.eval()
-    # # net, name, ver_list, delta=1e-6, rtol=1e-5
+    op = Net()
+    op.eval()
+    # net, name, ver_list, delta=1e-6, rtol=1e-5
+    kh, kw = 3, 3
+    input = paddle.rand((8, 1, 28, 28))
+    offset = paddle.rand((8, 2 * kh * kw, 26, 26))
+    mask = paddle.rand((8, kh * kw, 26, 26))
+    weight = paddle.rand((16, 1, kh, kw))
     # obj = APIOnnx(op, "deform_conv2d", [19])
-    # kh, kw = 3, 3
-    # input = paddle.rand((8, 1, 28, 28))
-    # offset = paddle.rand((8, 2 * kh * kw, 26, 26))
-    # mask = paddle.rand((8, kh * kw, 26, 26))
-    # weight = paddle.rand((16, 1, kh, kw))
     # obj.set_input_data("input_data", input, offset, weight, mask)
     # obj.run()
 
