359a38cd998b7d722f91d131176ba406037b51ee
jiuyuedeyu156 <jiuyuedeyuao@gmail.com>
2024-06-17 10:39:11 +0800

    fix bitwise_and op

--- paddle2onnx/mapper/tensor/bitwise_and.cc
+++ paddle2onnx/mapper/tensor/bitwise_and.cc
@@ -13,42 +13,77 @@
 // limitations under the License.
 
 #include "paddle2onnx/mapper/tensor/bitwise_and.h"
+#include "bitwise_and.h"
 
 namespace paddle2onnx {
 REGISTER_MAPPER(bitwise_and, BitWiseAndMapper)
 
 int32_t BitWiseAndMapper::GetMinOpset(bool verbose) {
-  constexpr int op_version = 18;
+  int op_version = 7;
+  auto input_info = GetInput("X");
+  if(input_info[0].dtype == P2ODataType::BOOL){
+    op_version = 7;
+    P2OLogger() << "For bool types, we recommend converting bitwise_and (opset v18) to logical_and (opset v7)"<<std::endl;
+    Logger(verbose, op_version) << RequireOpset(op_version) << std::endl;
+    return op_version;
+  }
+  op_version = 18;
   Logger(verbose, op_version) << RequireOpset(op_version) << std::endl;
   return op_version;
+}
+void BitWiseAndMapper::Opset7(){
+  auto x_info = GetInput("X");
+  auto y_info = GetInput("Y");
+  auto out_info = GetOutput("Out");
+  helper_->MakeNode( "And" ,{x_info[0].name, y_info[0].name}, {out_info[0].name});
 }
 void BitWiseAndMapper::Opset18() {
   auto x_info = GetInput("X");
   auto y_info = GetInput("Y");
   auto out_info = GetOutput("Out");
-  constexpr std::array<P2ODataType, 5> T = {
-    P2ODataType::INT16,
-    P2ODataType::INT32,
-    P2ODataType::INT64,
-    P2ODataType::INT8,
-    P2ODataType::UINT8,
-    // Except BOOL. Cast to UINT8.
-  };
-  std::string x_name = x_info[0].name;
-  std::string y_name = y_info[0].name;
-  bool is_cast = false;
-  constexpr int cast_dtype = P2ODataType::UINT8;
-  if (std::find(T.begin(), T.end(), x_info[0].dtype) == T.end()) {
-    // Since x and y are of the same type, we only need to determine the type of x
-    x_name = helper_->AutoCast(x_name, x_info[0].dtype, cast_dtype); // cast 
-    y_name = helper_->AutoCast(y_name, y_info[0].dtype, cast_dtype); // cast
-    is_cast = true;
-  } 
-  if(is_cast){
-    auto bitwise_and_node = helper_->MakeNode("BitwiseAnd", {x_name, y_name});
-    helper_->AutoCast(bitwise_and_node->output(0), out_info[0].name, cast_dtype, out_info[0].dtype);
+  if(x_info[0].dtype == P2ODataType::BOOL){
+    Warn()<<"There is a bool type input in Bitwise_and (opset v18), try to use logical_and (opset v7)"<<std::endl;
+    Opset7();
     return;
   }
-  helper_->MakeNode("BitwiseAnd", {x_name, y_name},{out_info[0].name});
+  helper_->MakeNode("BitwiseAnd", {x_info[0].name, y_info[0].name},{out_info[0].name});
 }
-}  // namespace paddle2onnx
+}// namespace paddle2onnx
+
+
+
+// void BitWiseAndMapper::Opset18() {
+//   auto x_info = GetInput("X");
+//   auto y_info = GetInput("Y");
+//   auto out_info = GetOutput("Out");
+//   constexpr std::array<P2ODataType, 5> T = {
+//     P2ODataType::INT16,
+//     P2ODataType::INT32,
+//     P2ODataType::INT64,
+//     P2ODataType::INT8,
+//     P2ODataType::UINT8,
+//     // Except BOOL. Cast to UINT8.
+//   };
+//   if(x_info[0].dtype == P2ODataType::BOOL){
+//     Warn()<<"There is a bool type input in Bitwise_and"<<std::endl;
+//     Opset14();
+//     return;
+//   }
+//   std::string x_name = x_info[0].name;
+//   std::string y_name = y_info[0].name;
+//   bool is_cast = false;
+//   constexpr int cast_dtype = P2ODataType::UINT8;
+//   if (std::find(T.begin(), T.end(), x_info[0].dtype) == T.end()) {
+//     // Since x and y are of the same type, we only need to determine the type of x
+//     x_name = helper_->AutoCast(x_name, x_info[0].dtype, cast_dtype); // cast 
+//     y_name = helper_->AutoCast(y_name, y_info[0].dtype, cast_dtype); // cast
+//     is_cast = true;
+//   } 
+//   if(is_cast){
+//     auto bitwise_and_node = helper_->MakeNode("BitwiseAnd", {x_name, y_name});
+//     helper_->AutoCast(bitwise_and_node->output(0), out_info[0].name, cast_dtype, out_info[0].dtype);
+//     return;
+//   }
+//   helper_->MakeNode("BitwiseAnd", {x_name, y_name},{out_info[0].name});
+// }
+// }  // namespace paddle2onnx
