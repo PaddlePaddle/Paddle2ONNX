56509cf694d3258317cb4afdc3d966c866f7b0b0
Zheng-Bicheng <zheng_bicheng@outlook.com>
2024-07-12 16:58:45 +0800

    update onnx ifelse

--- tests/onnx/export_if.py
+++ tests/onnx/export_if.py
@@ -3,22 +3,55 @@ import numpy as np
 from base_expect import expect
 
 if __name__ == "__main__":
-    node = onnx.helper.make_node(
-        "Identity",
-        inputs=["x"],
-        outputs=["y"],
+    # Given a bool scalar input cond.
+    # return constant tensor x if cond is True, otherwise return constant tensor y.
+
+    then_out = onnx.helper.make_tensor_value_info(
+        "then_out", onnx.TensorProto.FLOAT, [5]
+    )
+    else_out = onnx.helper.make_tensor_value_info(
+        "else_out", onnx.TensorProto.FLOAT, [5]
     )
 
-    data = np.array(
-        [
-            [
-                [
-                    [1, 2],
-                    [3, 4],
-                ]
-            ]
-        ],
-        dtype=np.float32,
+    x = np.array([1, 2, 3, 4, 5]).astype(np.float32)
+    y = np.array([5, 4, 3, 2, 1]).astype(np.float32)
+
+    then_const_node = onnx.helper.make_node(
+        "Constant",
+        inputs=[],
+        outputs=["then_out"],
+        value=onnx.numpy_helper.from_array(x),
     )
 
-    expect(node, inputs=[data], outputs=[data], name="test_identity")
+    else_const_node = onnx.helper.make_node(
+        "Constant",
+        inputs=[],
+        outputs=["else_out"],
+        value=onnx.numpy_helper.from_array(y),
+    )
+
+    then_body = onnx.helper.make_graph(
+        [then_const_node], "then_body", [], [then_out]
+    )
+
+    else_body = onnx.helper.make_graph(
+        [else_const_node], "else_body", [], [else_out]
+    )
+
+    if_node = onnx.helper.make_node(
+        "If",
+        inputs=["cond"],
+        outputs=["res"],
+        then_branch=then_body,
+        else_branch=else_body,
+    )
+
+    cond = np.array(1).astype(bool)
+    res = x if cond else y
+    expect(
+        if_node,
+        inputs=[cond],
+        outputs=[res],
+        name="test_if",
+        opset_imports=[onnx.helper.make_opsetid("", 11)],
+    )
