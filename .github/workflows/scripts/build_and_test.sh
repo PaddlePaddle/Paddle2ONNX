f6d1e29363f18d5769fc73d041c60e915e32874a
risemeup1 <515586620@qq.com>
2024-08-10 10:09:36 +0000

    fix

--- .github/workflows/scripts/build_and_test.sh
+++ .github/workflows/scripts/build_and_test.sh
@@ -2,8 +2,7 @@
 
 #build protobuf
 git clone https://github.com/protocolbuffers/protobuf.git
-cd protobuf
-git submodule update --init
+cd protobuf && git submodule update --init
 git checkout v4.22.0
 mkdir build_source && cd build_source
 cmake ../cmake -DCMAKE_INSTALL_PREFIX=${PWD}/installed_protobuf_lib -Dprotobuf_BUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
