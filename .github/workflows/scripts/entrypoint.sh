0de85e3f5d0d0ecce270ffd55db837bbcb89f025
risemeup1 <515586620@qq.com>
2024-08-13 03:18:48 +0000

    fix

--- .github/workflows/scripts/entrypoint.sh
+++ .github/workflows/scripts/entrypoint.sh
@@ -98,6 +98,7 @@ ignore="test_auto_scan_multiclass_nms.py
         test_index_select.py \
         test_prelu.py \
         test_repeat_interleave.py \
+        test_auto_scan_pool_adaptive_avg_ops.py \
         test_unique.py"
 bug=0
 
