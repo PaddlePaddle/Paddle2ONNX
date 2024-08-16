fa4cd18442648fa9d6149e3ddabd12a326160823
risemeup1 <515586620@qq.com>
2024-08-16 08:04:23 +0000

    fix

--- .github/workflows/scripts/download_paddle.sh
+++ .github/workflows/scripts/download_paddle.sh
@@ -14,7 +14,7 @@ if [ "$OS" = "Linux" ]; then
         echo "When the operating system is Linux, the system architecture only supports (x86_64 and aarch64), but the current architecture is $ARCH."
         exit 1
     fi
-    paddle_url="https://bj.bcebos.com/paddle2onnx/third_party/$paddle_tgz_name"
+    paddle_url="https://paddle2onnx.bj.bcebos.com/third_libs//$paddle_tgz_name"
 # Check if the operating system is Darwin (macOS)
 elif [ "$OS" = "Darwin" ]; then
     if [[ "$ARCH" == "x86_64" ]]; then
@@ -25,7 +25,7 @@ elif [ "$OS" = "Darwin" ]; then
       echo "When the operating system is Darwin, the system architecture only supports (x86_64 and arm64), but the current architecture is $ARCH."
       exit 1
     fi
-    paddle_url="https://bj.bcebos.com/paddle2onnx/third_libs/$paddle_tgz_name"
+    paddle_url="https://paddle2onnx.bj.bcebos.com/third_libs/$paddle_tgz_name"
 else
    echo "The system only supports (Linux and Darwin), but the current system is $OS."
 fi
