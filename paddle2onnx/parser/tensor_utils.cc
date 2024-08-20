85dc0747ed16884fb3cfda0a0cfb41b0c9aa7367
risemeup1 <515586620@qq.com>
2024-08-20 08:42:40 +0000

    fix

--- paddle2onnx/parser/tensor_utils.cc
+++ paddle2onnx/parser/tensor_utils.cc
@@ -1,37 +1,50 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "paddle2onnx/parser/tensor_utils.h"
 
 namespace paddle2onnx {
 
-  //TensorInfo
-  int64_t TensorInfo::Rank() const { return static_cast<int64_t>(shape.size()); }
-  TensorInfo::TensorInfo(const std::string& _name, const std::vector<int64_t>& _shape,
-             const int32_t& _dtype) {
-    name = _name;
-    shape.assign(_shape.begin(), _shape.end());
-    dtype = _dtype;
-  }
-  TensorInfo::TensorInfo(const TensorInfo& info) {
-    name = info.name;
-    shape.assign(info.shape.begin(), info.shape.end());
-    dtype = info.dtype;
-    is_tensor_array = info.is_tensor_array;
+int64_t TensorInfo::Rank() const { return static_cast<int64_t>(shape.size()); }
+TensorInfo::TensorInfo(const std::string &_name,
+                       const std::vector<int64_t> &_shape,
+                       const int32_t &_dtype) {
+  name = _name;
+  shape.assign(_shape.begin(), _shape.end());
+  dtype = _dtype;
+}
+TensorInfo::TensorInfo(const TensorInfo &info) {
+  name = info.name;
+  shape.assign(info.shape.begin(), info.shape.end());
+  dtype = info.dtype;
+  is_tensor_array = info.is_tensor_array;
+}
+
+// Weight
+template <typename T>
+void Weight::set(int32_t data_type, const std::vector<int64_t> &dims,
+                 const std::vector<T> &data) {
+  buffer.clear();
+  shape.clear();
+  dtype = data_type;
+  buffer.resize(data.size() * PaddleDataTypeSize(dtype));
+  memcpy(buffer.data(), data.data(), data.size() * PaddleDataTypeSize(dtype));
+  for (auto &d : dims) {
+    shape.push_back(d);
   }
 
-    //Weight
-    template <typename T>
-    void Weight::set(int32_t data_type, const std::vector<int64_t>& dims,
-           const std::vector<T>& data) {
-    buffer.clear();
-    shape.clear();
-    dtype = data_type;
-    buffer.resize(data.size() * PaddleDataTypeSize(dtype));
-    memcpy(buffer.data(), data.data(), data.size() * PaddleDataTypeSize(dtype));
-    for (auto& d : dims) {
-      shape.push_back(d);
-    }
-
-    template <typename T>
-  void Weight::get(std::vector<T>* data) const {
+  template <typename T> void Weight::get(std::vector<T> * data) const {
     int64_t nums = std::accumulate(std::begin(shape), std::end(shape), 1,
                                    std::multiplies<int64_t>());
     data->resize(nums);
@@ -60,29 +73,29 @@ namespace paddle2onnx {
              "Weight::get() only support int64/int32/int8/float32/float64.");
     }
   }
-  }
-    int32_t PaddleDataTypeSize(int32_t paddle_dtype) {
-        if (paddle_dtype == P2ODataType::BOOL) {
-            return sizeof(bool);
-        } else if (paddle_dtype == P2ODataType::INT8) {
-            return sizeof(int8_t);
-        } else if (paddle_dtype == P2ODataType::INT16) {
-            return sizeof(int16_t);
-        } else if (paddle_dtype == P2ODataType::INT32) {
-            return sizeof(int32_t);
-        } else if (paddle_dtype == P2ODataType::INT64) {
-            return sizeof(int64_t);
-        } else if (paddle_dtype == P2ODataType::FP32) {
-            return sizeof(float);
-        } else if (paddle_dtype == P2ODataType::FP16) {
-            return sizeof(int16_t);
-        } else if (paddle_dtype == P2ODataType::FP64) {
-            return sizeof(double);
-        } else if (paddle_dtype == P2ODataType::UINT8) {
-            return sizeof(uint8_t);
-        } else {
-            Assert(false, "Unexpected data type: " + std::to_string(paddle_dtype));
-        }
-        return -1;
-    }
 }
+int32_t PaddleDataTypeSize(int32_t paddle_dtype) {
+  if (paddle_dtype == P2ODataType::BOOL) {
+    return sizeof(bool);
+  } else if (paddle_dtype == P2ODataType::INT8) {
+    return sizeof(int8_t);
+  } else if (paddle_dtype == P2ODataType::INT16) {
+    return sizeof(int16_t);
+  } else if (paddle_dtype == P2ODataType::INT32) {
+    return sizeof(int32_t);
+  } else if (paddle_dtype == P2ODataType::INT64) {
+    return sizeof(int64_t);
+  } else if (paddle_dtype == P2ODataType::FP32) {
+    return sizeof(float);
+  } else if (paddle_dtype == P2ODataType::FP16) {
+    return sizeof(int16_t);
+  } else if (paddle_dtype == P2ODataType::FP64) {
+    return sizeof(double);
+  } else if (paddle_dtype == P2ODataType::UINT8) {
+    return sizeof(uint8_t);
+  } else {
+    Assert(false, "Unexpected data type: " + std::to_string(paddle_dtype));
+  }
+  return -1;
+}
+}  // namespace paddle2onnx
--- paddle2onnx/parser/tensor_utils.h
+++ paddle2onnx/parser/tensor_utils.h
@@ -1,16 +1,33 @@
+// Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
+#include "paddle2onnx/parser/tensor_utils.h"
+
+#include <cstddef>
 #include <cstdint>
+
 #include <algorithm>
 #include <cassert>
-#include <numeric>
-#include <type_traits>
-#include <string>
-#include <vector>
 #include <cstring>
-#include "paddle2onnx/utils/utils.h"
+#include <numeric>
+#include <string>
+#include <type_traits>
+#include <vector>
 
-namespace paddle2onnx{
-    enum P2ODataType {
+namespace paddle2onnx {
+enum P2ODataType {
   BOOL,
   INT16,
   INT32,
@@ -44,10 +61,10 @@ struct TensorInfo {
   bool is_tensor_array = false;
 
   TensorInfo() {}
-  TensorInfo(const std::string& _name, const std::vector<int64_t>& _shape,
-             const int32_t& _dtype);
+  TensorInfo(const std::string &_name, const std::vector<int64_t> &_shape,
+             const int32_t &_dtype);
 
-  TensorInfo(const TensorInfo& info);
+  TensorInfo(const TensorInfo &info);
 };
 
 struct Weight {
@@ -56,11 +73,10 @@ struct Weight {
   int32_t dtype;
 
   template <typename T>
-  void set(int32_t data_type, const std::vector<int64_t>& dims,
-           const std::vector<T>& data);
+  void set(int32_t data_type, const std::vector<int64_t> &dims,
+           const std::vector<T> &data);
 
-  template <typename T>
-  void get(std::vector<T>* data) const;
+  template <typename T> void get(std::vector<T> *data) const;
 };
 
-}
+}  // namespace paddle2onnx
