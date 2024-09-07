1cd844c0152ddd93b82fdeff0527476407475507
risemeup1 <515586620@qq.com>
2024-09-07 10:40:49 +0000

    fix

--- tools/scripts/paddle2onnx_build.sh
+++ tools/scripts/paddle2onnx_build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -x
 set -e
 PADDLE2ONNX_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")/../../../" && pwd )"
@@ -56,7 +71,7 @@ function build_paddle2onnx(){
     cd ${PADDLE2ONNX_ROOT}
     $1 -m pip install --upgrade pip
     $1 -m pip install build
-    $1 -m pip install --pre paddlepaddle -i https://www.paddlepaddle.org.cn/packages/nightly/cpu/
+    # $1 -m pip install --pre paddlepaddle -i https://www.paddlepaddle.org.cn/packages/nightly/cpu/
 
     #build paddle2onnx
     export PIP_EXTRA_INDEX_URL="https://www.paddlepaddle.org.cn/packages/nightly/cpu/"
