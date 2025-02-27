d0a5ed5d467ec8a0726ce93b07b1579cc047c401
zhanghonggeng <zhanghonggeng@baidu.com>
2025-02-27 07:47:13 +0000

    fix gaussian_random mapper

--- tests/run.sh
+++ tests/run.sh
@@ -30,7 +30,6 @@ ignore="test_auto_scan_multiclass_nms.py
         test_auto_scan_pad2d.py \
         test_auto_scan_uniform_random_batch_size_like.py \
         test_auto_scan_uniform_random.py \
-        test_auto_scan_gaussian_random.py \
         test_auto_scan_distribute_fpn_proposals1.py \
         test_auto_scan_distribute_fpn_proposals_v2.py \
         test_auto_scan_fill_constant_batch_size_like.py \
