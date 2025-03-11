b4d9db8203e58f51b8d8b0ba368ed3efeec01c25
zhanghonggeng <zhanghonggeng@baidu.com>
2025-03-11 09:18:52 +0000

    update

--- tests/run.sh
+++ tests/run.sh
@@ -25,22 +25,21 @@
 #tar xf quantized_models.tar.gz
 
 cases=$(find . -name "test*.py" | sort)
-ignore="test_auto_scan_multiclass_nms.py
-        test_auto_scan_roi_align.py \ # need to be rewrite
-        test_auto_scan_uniform_random_batch_size_like.py \
-        test_auto_scan_uniform_random.py \
-        test_auto_scan_distribute_fpn_proposals1.py \
-        test_auto_scan_distribute_fpn_proposals_v2.py \
-        test_auto_scan_generate_proposals.py \
-        test_uniform.py \
-        test_deform_conv2d.py \
-        test_nn_GRU.py \
+ignore="test_auto_scan_multiclass_nms.py                        # input shuold be xxx, but received Value
+        test_auto_scan_roi_align.py \                           # need to be rewrite
+        test_auto_scan_uniform_random_batch_size_like.py \      # due to the different random number engines, the generated random numbers cannot be compared
+        test_auto_scan_uniform_random.py \                      # same as uniform_random_batch_size_like
+        test_auto_scan_distribute_fpn_proposals1.py \           # need to be rewrite, There is no distribute_fpn_proposals Mapper
+        test_auto_scan_distribute_fpn_proposals_v2.py \         # same as distribute_fpn_proposals1
+        test_auto_scan_generate_proposals.py \                  # need to be rewrite, There is no generate_proposals Mapper
+        test_uniform.py \                                       # same as uniform_random_batch_size_like
+        test_deform_conv2d.py \                                 # onnxruntime is not supported right now
         test_quantize_model.py \
         test_quantize_model_minist.py \
-        test_auto_scan_partial_ops.py \
+        test_auto_scan_partial_ops.py \                         # input shuold be xxx, but received Value
         test_dygraph2onnx.py \
-        test_auto_scan_dequantize_linear.py \
-        test_auto_scan_quantize_linear.py \
+        test_auto_scan_dequantize_linear.py \                   # input shuold be xxx, but received Value
+        test_auto_scan_quantize_linear.py \                     # input shuold be xxx, but received Value
         test_quantize_model_speedup.py \
         test_resnet_fp16.py"
 bug=0
